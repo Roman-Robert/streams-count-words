936539af5469c0c08612b75af8cbbf162f26ed8e
Roman-Robert <116169632+Roman-Robert@users.noreply.github.com>
2023-03-09 18:53:24 +0200

    task solution

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,12 +1,33 @@
 package com.epam.rd.autotasks;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
 
-        return null;
+        List<String> words = lines.stream()
+                .map(line -> line.toLowerCase()
+                        .replaceAll("[.,]", "")
+                        .split(" "))
+                .flatMap(Arrays :: stream)
+                .filter(word -> word.length() >= 4)
+                .collect(Collectors.groupingBy(str -> str, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(w -> w.getValue() >=10 )
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(result -> String.format("%s - %d", result.getKey(), result.getValue()))
+                .collect(Collectors.toList());
+
+
+        return words.toString()
+                .replaceAll(", ", "\n")
+                .replaceAll("[\\],\\[]", "");
     }
 }
